26f4e552790bf4b0066b016a58ec383f8ae7c2d4
Shen8Singh <gayashan9090@gmail.com>
2024-08-20 06:40:42 +0530

    almost done

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,58 +2,73 @@ import React, { useState } from 'react';
 import './Calculator.css'; // Create this CSS file for styling
 
 function Calculator() {
-  const [result, setResult] = useState('');
+    const [result, setResult] = useState('');
+    const [inputText, setInputText] = useState('');
 
-  const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
-  };
+    const handleClick = (e) => {
+        setInputText(inputText.concat(e.target.name));
+        setResult(''); // Clear the result when a new input is added
+    };
 
-  const clear = () => {
-    setResult('');
-  };
+    const clear = () => {
+        setInputText('');
+        setResult('');
+    };
 
-  const backspace = () => {
-    setResult(result.slice(0, -1));
-  };
+    const backspace = () => {
+        setInputText(inputText.slice(0, -1));
+    };
 
-  const calculate = () => {
-    try {
-      setResult(eval(result).toString());
-    } catch (err) {
-      setResult('Error');
-    }
-  };
+    const calculate = () => {
+        try {
+            const calculatedResult = eval(inputText).toString();
+            setResult(calculatedResult);
+        } catch (err) {
+            setResult('Error');
+        }
+    };
 
-  return (
-    <div className="calculator">
-      <form>
-        <input type="text" className='textInput'  value={""} readOnly />
-        <input type="text" className='resultInput' value={result} readOnly />
-      </form>
-      <div className="keypad">
-        <button onClick={clear} id="clear" className='symble'>C</button>
-        <button name="[" onClick={handleClick} className='symble'>[</button>
-        <button name="]" onClick={handleClick} className='symble'>]</button>
-        <button name="/" onClick={handleClick} className='symble'>&divide;</button>
-        <button name="7" onClick={handleClick} className='number'>7</button>
-        <button name="8" onClick={handleClick} className='number'>8</button>
-        <button name="9" onClick={handleClick} className='number'>9</button>
-        <button name="*" onClick={handleClick} className='symble'>&times;</button>
-        <button name="4" onClick={handleClick} className='number'>4</button>
-        <button name="5" onClick={handleClick} className='number'>5</button>
-        <button name="6" onClick={handleClick} className='number'>6</button>
-        <button name="-" onClick={handleClick} className='symble'>&ndash;</button>
-        <button name="1" onClick={handleClick} className='number'>1</button>
-        <button name="2" onClick={handleClick} className='number'>2</button>  
-        <button name="3" onClick={handleClick}className='number'>3</button>
-        <button name="+" onClick={handleClick} className='symble'>+</button>
-        <button name="00" onClick={handleClick} className='number'>00</button>
-        <button name="0" onClick={handleClick} className='number'>0</button>
-        <button name="." onClick={handleClick}className='symble' id='dot'>.</button>
-        <button onClick={calculate} id="result">=</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="calculator">
+            <form>
+                <input
+                    type="text"
+                    className='textInput'
+                    value={inputText}
+                    readOnly
+                    id='inputText'
+                />
+                <input
+                    type="text"
+                    className='resultInput'
+                    value={result}
+                    readOnly
+                />
+            </form>
+            <div className="keypad">
+                <button onClick={clear} id="clear" className='symble'>C</button>
+                <button name="[" onClick={handleClick} className='symble'>[</button>
+                <button name="]" onClick={handleClick} className='symble'>]</button>
+                <button name="/" onClick={handleClick} className='symble'>&divide;</button>
+                <button name="7" onClick={handleClick} className='number'>7</button>
+                <button name="8" onClick={handleClick} className='number'>8</button>
+                <button name="9" onClick={handleClick} className='number'>9</button>
+                <button name="*" onClick={handleClick} className='symble'>&times;</button>
+                <button name="4" onClick={handleClick} className='number'>4</button>
+                <button name="5" onClick={handleClick} className='number'>5</button>
+                <button name="6" onClick={handleClick} className='number'>6</button>
+                <button name="-" onClick={handleClick} className='symble'>&ndash;</button>
+                <button name="1" onClick={handleClick} className='number'>1</button>
+                <button name="2" onClick={handleClick} className='number'>2</button>  
+                <button name="3" onClick={handleClick} className='number'>3</button>
+                <button name="+" onClick={handleClick} className='symble'>+</button>
+                <button name="00" onClick={handleClick} className='number'>00</button>
+                <button name="0" onClick={handleClick} className='number'>0</button>
+                <button name="." onClick={handleClick} className='symble' id='dot'>.</button>
+                <button onClick={calculate} id="result">=</button>
+            </div>
+        </div>
+    );
 }
 
 export default Calculator;
